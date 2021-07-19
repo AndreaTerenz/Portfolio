a572f47d0732f32f82b67a8d9d84b6c02717433e
andrea <andrea.terenziani.at@gmail.com>
2021-07-19 23:08:55 +0200

    tried a p5 animated background

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,33 @@
-// Find the latest version by visiting https://cdn.skypack.dev/three.
-import * as THREE from 'https://cdn.skypack.dev/three@0.130.1';
+var size
+var rows = 10
+var columns = 20
+var offset = 0.0
 
-const scene = new THREE.Scene();
+function setup() {
+    console.log("AAAAAAAAAAAAAAAAAa")
+    createCanvas(displayWidth, displayHeight).parent("#p5Parent")
+    size = createVector(displayWidth, displayHeight)
+}
+
+function draw() {
+    background(0);
+
+    let colSize = Math.floor(size.x / columns)
+    let rowSize = Math.floor(size.y / rows)
+    stroke(255)
+    strokeWeight(5)
+    for (let x = 0; x < columns; x += 1) {
+        for (let y = 0; y < rows; y += 1) {
+            let pos = createVector((x + .5), (y + .5))
+            stroke(255 * noise(pos.x, pos.y, offset))
+            point(pos.x * colSize, pos.y * rowSize)
+        }
+    }
+
+    offset += .01
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    size = createVector(displayWidth, displayHeight)
+}
