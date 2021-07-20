e15a8e4e2d987ec2fa8504fcca37b775b8abe9d3
andrea <andrea.terenziani.at@gmail.com>
2021-07-20 17:12:40 +0200

    Added animated background with p5

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,46 @@
 var size
-var rows = 10
-var columns = 20
+var ratio
+var rows = 30
+var columns
 var offset = 0.0
 
 function setup() {
-    console.log("AAAAAAAAAAAAAAAAAa")
     createCanvas(displayWidth, displayHeight).parent("#p5Parent")
+    ratio = displayWidth / displayHeight
     size = createVector(displayWidth, displayHeight)
+    columns = rows  * ratio
+    rectMode(CENTER)
 }
 
 function draw() {
     background(0);
 
-    let colSize = Math.floor(size.x / columns)
-    let rowSize = Math.floor(size.y / rows)
-    stroke(255)
-    strokeWeight(5)
+    let colSize = Math.floor(size.x*2 / columns)
+    let rowSize = Math.floor(size.y*2 / rows)
+
+    strokeWeight(0)
     for (let x = 0; x < columns; x += 1) {
         for (let y = 0; y < rows; y += 1) {
             let pos = createVector((x + .5), (y + .5))
-            stroke(255 * noise(pos.x, pos.y, offset))
-            point(pos.x * colSize, pos.y * rowSize)
+            let noiseScale = .1
+            let noiseVal = noise(pos.x*noiseScale, pos.y*noiseScale, offset)
+
+            let startCol = color('rgb(19,15,118)')
+            let endCol = color('rgb(75,255,255)')
+
+            fill(lerpColor(startCol, endCol, noiseVal))
+            let size = createVector(colSize, rowSize)
+            rect(pos.x * colSize, pos.y * rowSize, size.x, size.y)
         }
     }
 
-    offset += .01
+    offset += .005
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    background(0)
+    resizeCanvas(windowWidth, windowHeight)
+    ratio = displayWidth / displayHeight
     size = createVector(displayWidth, displayHeight)
+    rows = columns * ratio
 }
