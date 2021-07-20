be093d30aa8e704219d3ff9d6cf896bf6da3d162
andrea <andrea.terenziani.at@gmail.com>
2021-07-20 23:23:05 +0200

    Deactivated background animation :(

--- scripts/main.js
+++ scripts/main.js
@@ -5,19 +5,21 @@ var columns
 var offset = 0.0
 
 function setup() {
-    createCanvas(displayWidth, displayHeight, WEBGL).parent("#p5Parent")
+    createCanvas(displayWidth, displayHeight).parent("#p5Parent")
+    drawSquares()
+}
+
+function drawSquares() {
     ratio = displayWidth / displayHeight
     size = createVector(displayWidth, displayHeight)
     columns = rows * ratio
     rectMode(CENTER)
-}
-
-function draw() {
-    background(0);
 
     let colSize = Math.floor(size.x * 2 / columns)
     let rowSize = Math.floor(size.y * 2 / rows)
 
+    push()
+    translate(-displayWidth / 2, -displayHeight / 2, 0)
     strokeWeight(0)
     for (let x = 0; x < columns; x += 1) {
         for (let y = 0; y < rows; y += 1) {
@@ -33,14 +35,9 @@ function draw() {
             rect(pos.x * colSize - displayWidth / 2, pos.y * rowSize - displayHeight / 2, size.x, size.y)
         }
     }
-
-    offset += .005
+    pop()
 }
 
 function windowResized() {
-    background(0)
-    resizeCanvas(windowWidth, windowHeight)
-    ratio = displayWidth / displayHeight
-    size = createVector(displayWidth, displayHeight)
-    rows = columns * ratio
+    drawSquares()
 }
